15e72abfcedf27336e6110d55424f80492e6b9d7
SachinsKhade <93199942+SachinsKhade@users.noreply.github.com>
2021-10-27 13:37:27 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,13 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+int a=10;
+int b=11;
+int c=a+b;
+System.out.println(c);
+	}
+
+}
