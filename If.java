e0fa82637a67b182effdadbdad3ab4d8a1fd6b02
SachinsKhade <93199942+SachinsKhade@users.noreply.github.com>
2021-10-28 18:31:03 +0530

    Add files via upload

---
+++ If.java
@@ -0,0 +1,16 @@
+package ControlFlowStatements;
+
+public class If {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		if (a + b > 20) {
+
+			System.out.println("x+y is greaterthan 20 ");
+
+		}
+
+	}
+}
