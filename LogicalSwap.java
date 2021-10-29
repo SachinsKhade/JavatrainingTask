4f9458b5b5b9652874e82f285bbf4a5f203fb2fc
SachinsKhade <93199942+SachinsKhade@users.noreply.github.com>
2021-10-29 19:41:35 +0530

    Add files via upload

---
+++ LogicalSwap.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class LogicalSwap {
+
+	public static void main(String[] args) {
+		// Swapping number
+		int a = 10, b = 20;
+
+		System.out.println("Before swapping  number " + a + " " + b);
+		int t = a;
+		a = b;
+		b = t;
+		System.out.println("Afore swapping  number " + a + " " + b);
+
+	}
+
+}
