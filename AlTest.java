c70876041b215d67a2deaa59a1cfa6b3e0511ddd
SachinsKhade <93199942+SachinsKhade@users.noreply.github.com>
2021-11-11 18:08:20 +0530

    Add files via upload

---
+++ AlTest.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class AlTest {
+
+	public static void main(String[] args) {
+		// create ArrayList
+		ArrayList<String> languages = new ArrayList<>();
+
+		// Add elements to ArrayList
+		languages.add("Java");
+		languages.add("Python");
+		languages.add("Swift");
+		System.out.println("ArrayList: " + languages);
+
+	}
+
+}
---
+++ Aladd.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.*;
+
+public class Aladd {
+
+	public static void main(String[] args) {
+		// create ArrayList
+		ArrayList<String> languages = new ArrayList<>();
+
+		// add() method without the index parameter
+		languages.add("Java");
+		languages.add("C");
+		languages.add("Python");
+		System.out.println("ArrayList: " + languages);
+
+	}
+
+}
